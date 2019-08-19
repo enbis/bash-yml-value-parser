17f157b83e9f9c4d3a482d394ae30e2f8504ddce
Enrico <enrico.bison@spindoxlabs.it>
2019-08-20 00:14:36 +0200

    function iteration improved

--- function.sh
+++ function.sh
@@ -15,51 +15,36 @@ _get_config_value() {
 
     cat config_2.yml | while read line; do
         #v="$(echo $line | tr --delete :)"
-        echo "$line"
         count_lines=$(($count_lines+1))
         if [[ "$line" == $1* ]]; then
-            # IFS=':' read -r -a array <<< "$line"
-            # res="$(echo -e "${array[1]}" | tr -d '[:space:]')"
             read ADDR1 ADDR2 <<< $(IFS=":"; echo $line)
-            echo ${ADDR1}
-            echo ${ADDR2}
             if test -z $ADDR2; then
-                iteration "$count_lines"
+                retval=$(iteration $count_lines)
+                echo $retval
             else 
                 echo ${ADDR2}
-                echo $count_lines
             fi
             break
         fi
     done
-
-    # if [ -z "${res}"]
-    # then
-    #     res=()
-    #     top=false
-    #     cat config_2.yml | while read line; do
-    #         echo $line
-    #         if [[ $line = $1 ]] 
-    #         then
-    #             echo "trovato"
-    #             $top=true 
-    #             continue
-    #         fi
-    #         if [[ "$top"=true ]] 
-    #         then
-    #             res+=$line
-    #             echo $line
-    #         fi
-    #     done
-    #     echo ${res}
-    # else
-	#     echo ${res}
-    # fi
 }
 
 iteration()
-{   
-    echo "iteration from line $1"
+{
+    counter="0"
+    i=0
+    eof=0
+    cat config_2.yml | while read -r line; do
+        counter=$(($counter+1))
+        if [[ $counter -gt $1 ]]; then
+            if [[ $line = *-* ]]; then
+                array[$i]=$line
+                i=$(($i+1))
+            else
+                echo ${array[@]}
+            fi
+        fi
+    done  
 }
 
 $*
